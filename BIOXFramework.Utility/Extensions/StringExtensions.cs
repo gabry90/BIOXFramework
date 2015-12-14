d907ead6d35af8edd7f61844e26e2cb3b5c0223e
Gabriele Gindro <gabrygin90@hotmail.it>
2015-12-14 18:12:53 +0100

    [Utility] update extensions

--- BIOXFramework.Utility/Extensions/StringExtensions.cs
+++ BIOXFramework.Utility/Extensions/StringExtensions.cs
@@ -1,15 +1,13 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace BIOXFramework.Utility.Extensions
 {
     public static class StringExtensions
     {
-        public static bool IsNumeric(this Type type)
-        {
-            return type.IsValueType && type != typeof(bool) && type != typeof(Enum) && type != typeof(DateTime);
-        }
-
         public static string InLine(this string self, string replacedChar = " ")
         {
             if (replacedChar == null
@@ -20,6 +18,62 @@ namespace BIOXFramework.Utility.Extensions
                 || replacedChar.Contains("\r"))
                 replacedChar = " ";
             return Regex.Replace(self, @"[\f\n\t\v\r]", replacedChar);
+        }
+
+        public static string Substring(this string self, string from = null, string until = null, StringComparison comparison = StringComparison.InvariantCulture)
+        {
+            var fromLength = (from ?? string.Empty).Length;
+            var startIndex = !string.IsNullOrEmpty(from)
+                ? self.IndexOf(from, comparison) + fromLength
+                : 0;
+
+            if (startIndex < fromLength)
+                return null;
+
+            var endIndex = !string.IsNullOrEmpty(until)
+            ? self.IndexOf(until, startIndex, comparison)
+            : self.Length;
+
+            if (endIndex < 0)
+                return null;
+
+            var subString = self.Substring(startIndex, endIndex - startIndex);
+            return subString;
+        }
+
+        public static void Clear(this StringBuilder sb)
+        {
+            sb.Length = 0;
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return str == null || str.Trim() == "";
+        }
+
+        public static bool IsNumeric(this Type type)
+        {
+            return type.IsValueType && type != typeof(bool) && !type.IsEnum && type != typeof(DateTime);
+        }
+
+        public static List<string> SplitByLenght(this string self, int lenght)
+        {
+            if (lenght <= 0 || self.Length <= lenght)
+                return new List<string> { self };
+
+            return self
+                .Where((x, i) => i % lenght == 0)
+                .Select(
+                    (x, i) => new string(self
+                        .Skip(i * lenght)
+                        .Take(lenght)
+                        .ToArray()))
+                .ToList();
+        }
+
+        public static int CountWithCarriageReturn(this string self)
+        {
+            return self.Contains("\n") ? self.Length + self.Split('\n').Length - 1 : self.Length;
         }
     }
 }
